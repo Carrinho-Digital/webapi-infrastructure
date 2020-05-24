ca3eff714a10d75842e64b08a5c2f9bb9140da3c
eclesio <eclesiomelo.1@gmail.com>
2020-05-24 16:16:51 -0400

    keep default vpc name

--- digital_ocean/main.tf
+++ digital_ocean/main.tf
@@ -1,5 +1,5 @@
-resource "digitalocean_vpc" "carrinho_sfo1_vpc" {
-  name     = "carrinhodigital-sfo1-vpc"
+resource "digitalocean_vpc" "carrinho_vpc" {
+  name     = "carrinhodigital-vpc"
   region   = var.digital_ocean_region_sfo1
   ip_range = var.vpc_sfo1_ip_range
 }
@@ -18,7 +18,7 @@ resource "digitalocean_kubernetes_cluster" "kubernetes_prod_cluster" {
   tags     = ["prod", "webapi"]
 
   node_pool {
-    name       = "worker_pool"
+    name       = "worker-pool"
     size       = "s-1vcpu-2gb"
     auto_scale = true
     min_nodes  = 2
